4dff947bd962381c7f95e032f92dcb3939a687d3
Fahari <karonjekevin67@gmail.com>
2019-03-04 14:59:13 +0300

    create the first photo model

--- art/models.py
+++ art/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    photo_name = models.CharField(max_length =30)
+    description = models.TextField(max_length =120)
+    
