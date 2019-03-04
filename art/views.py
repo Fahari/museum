690246939aef76db7648dab58823c691562906f4
Fahari <karonjekevin67@gmail.com>
2019-03-04 17:03:04 +0300

    make the today/ route our site root route

--- art/views.py
+++ art/views.py
@@ -3,10 +3,7 @@ from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
-
-def photos_of_day(request):
+def photos_today(request):
     date = dt.date.today()
     return render(request, 'all-art/today-photos.html', {"date": date,})
 
