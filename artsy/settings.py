9733b424d3d02703df6fe251a50d2bdff2a720b7
Fahari <karonjekevin67@gmail.com>
2019-03-06 15:46:39 +0300

    commiting to heroku

--- artsy/settings.py
+++ artsy/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 # development
 if config('MODE')=="dev":
    DATABASES = {
